359f8c0335902cb7710824f1d79160e6446bb625
Pavle_io23@student.itstep.org <Pavle_io23@student.itstep.org>
2025-09-16 21:36:08 +0300

    123

--- LP/FileName.cpp
+++ LP/FileName.cpp
@@ -4,54 +4,42 @@ using namespace std;
 class Fraction
 {
 private:
-    int x; 
+    int x;
     int y;
-    char s; 
 
 public:
 
-    Fraction() 
+    Fraction()
     {
         cout << "Constructor by default\n";
         x = 0;
         y = 0;
-        s = 'A';
     }
-    Fraction(char z, int a, int b) 
+    Fraction( int a, int b)
     {
-        s = z;
         x = a;
         y = b;
         cout << "Constructor by 3 params\n";
     }
 
-    void Print() 
+    void Print()
     {
-        cout << s << "-> X: " << x << "\tY: " << y << endl;
+        cout << " X: " << x << "\tY: " << y << endl;
     }
-    void Init(char t, int _x, int _y) 
+    void Init(int _x, int _y)
     {
-        s = t;
         x = _x;
         y = _y;
     }
-    void SetSymbol(char syn)
-    {
-        s = syn;
-    }
-    char GetSymbol()
-    {
-        return s;
-    }
+    
 };
 int main()
 {
-    Fraction a;
-    a.Print();
-    a.Init('A', 10, -20);
+    Fraction a(15, -30);;
     a.Print();
 
-    Fraction b('B', 10, 20);
+
+    Fraction b( 10, 20);
     b.Print();
 
 
